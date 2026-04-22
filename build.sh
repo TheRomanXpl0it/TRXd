81783410bf8415460c9b33af2b0997ab0a13b313
simonedimaria <12881861+simonedimaria@users.noreply.github.com>
2026-04-23 00:53:47 +0200

    improved build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eux
+
+export GIT_HASH=$(git rev-parse HEAD)
+if ! docker network inspect trxd-shared-internal >/dev/null 2>&1; then
+	echo "[!] run scripts/net.sh first"; exit 1
+fi
+source .env && docker compose -f ./compose.yml down && docker compose -f ./compose.yml up --build -d
+echo "Git hash: $GIT_HASH"
+
+docker compose -f ./compose.yml logs -f
