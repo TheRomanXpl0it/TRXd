3d30de455a6f387d0455d25d979e4eee7673f1a9
simonedimaria <12881861+simonedimaria@users.noreply.github.com>
2026-04-24 19:28:01 +0200

    fixed build script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ export GIT_HASH=$(git rev-parse HEAD)
 if ! docker network inspect trxd-shared-internal >/dev/null 2>&1; then
 	echo "[!] run scripts/net.sh first"; exit 1
 fi
-source .env && docker compose -f ./compose.yml down && docker compose -f ./compose.yml up --build -d
+source .env && docker compose -f ./compose.yml build && docker compose -f ./compose.yml down && docker compose -f ./compose.yml up --build -d
 echo "Git hash: $GIT_HASH"
 
 docker compose -f ./compose.yml logs -f
